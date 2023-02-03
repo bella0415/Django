5963a1fef348cd39184420e242f680ebdc9c127c
bella <yiseuljeong0415@gmail.com>
2023-02-03 12:42:07 +0900

    delete all

--- website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [ 
-    path("text",  # url 주소 
-         views.web_test,  # 접속하면 함수 web_test 실행
-         name = "web_test"), # 이름 지정 
-]
