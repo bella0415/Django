e53343b818c0da881bef07d2b66eaef19d3d8794
bella <yiseuljeong0415@gmail.com>
2023-02-03 12:13:14 +0900

    update to test

--- sumsite/settings.py
+++ sumsite/settings.py
@@ -39,12 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "website",
-    "corsheaders" # corsheaders를 추가
 ]
 
 # middleware class도 추가
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -53,33 +51,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
-#################################################################################
-
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-
-CORS_ALLOW_METHODS = (
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-)
-
-CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-)
-###############################################################################
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
--- website/views.py
+++ website/views.py
@@ -5,5 +5,9 @@ def web_test(request) :
     url = 'http://43.200.179.186/'
     res = requests.get(url)
     text = res.json()
-    return render(request, 'index.html',{"text":text})
+    
+    if text == None :
+        return render(request,'index.html')
+    else :
+        return render(request, 'index.html',{"text":text})
 
