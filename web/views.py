d77adab6815db90031911b1442977a4ea86e2275
bella <yiseuljeong0415@gmail.com>
2023-02-04 23:28:36 +0900

    update html

--- web/views.py
+++ web/views.py
@@ -1,11 +1,8 @@
 import json
 import requests
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 def get_api(request) :
-    url = 'http://43.200.179.186/'
-    res = requests.get(url)
-    text = res.json()
-    return render(request,'index.html',{"text":text})
+    return render(request,'index.html')
         
