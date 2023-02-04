1cfaebf41c322917a8d1be12a2150795f46c0880
bella <yiseuljeong0415@gmail.com>
2023-02-05 01:27:30 +0900

    update html

--- web/urls.py
+++ web/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
    path("", 
         views.get_api, 
         name='get_api'),
+   path("code", # pk라는 int 형 변수 가 들어 감
+         views.get_code, 
+         name = "get_code"),
  ]
--- web/views.py
+++ web/views.py
@@ -5,4 +5,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 def get_api(request) :
     return render(request,'index.html')
+
+def get_code(request) :
+    return render(request, 'code.html')
         
