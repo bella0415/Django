592553f3cb5489ca620ab625a955677527ab92cf
bella <yiseuljeong0415@gmail.com>
2023-02-06 15:19:00 +0900

    update html files

--- web/urls.py
+++ web/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("", 
+     path("", 
         views.get_api, 
         name='get_api'),
-   path("code", # pk라는 int 형 변수 가 들어 감
+     path("code", 
          views.get_code, 
          name = "get_code"),
+     path("contact", # pk라는 int 형 변수 가 들어 감
+         views.get_contact, 
+         name = "get_contact"),
+   
+   
  ]
--- web/views.py
+++ web/views.py
@@ -3,9 +3,18 @@ import requests
 
 from django.shortcuts import render, get_object_or_404, redirect
 
+# email 전송
+from django.core.mail import EmailMessage
+
+# api 호출 페이지
 def get_api(request) :
     return render(request,'index.html')
 
+# code 페이지 
 def get_code(request) :
-    return render(request, 'code.html')
-        
+    return render(request, 'codes.html')
+
+# contact 페이지      
+def get_contact(request) :
+    return render(request, 'contact.html')
+
